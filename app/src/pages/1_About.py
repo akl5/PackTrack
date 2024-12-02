a0bfc6b2caa97e8eafc79775ead042aa974970b6
Alice Lee <86637906+akl5@users.noreply.github.com>
2024-12-01 19:08:32 -0500

    removed old about file

--- app/src/pages/1_About.py
+++ app/src/pages/1_About.py
@@ -6,3 +6,15 @@ from streamlit_extras.app_logo import add_logo
 from modules.nav import SideBarLinks
 
 SideBarLinks()
+
+st.write("# About this App")
+
+st.markdown (
+    """
+    This is a an unfinished page for PackTrack. 
+
+    The goal of this demo is to provide more information about PackTrack and the benefits that we offer to students as a platform. 
+
+    Stay tuned for more information and features to come!
+    """
+        )
