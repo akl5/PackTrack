0070b3a57ef398c687bcd4aa75d5d6a4420f5655
danielYZeng <166508723+danielYZeng@users.noreply.github.com>
2024-12-05 16:03:39 -0500

    added all columns to insert review content into

--- api/backend/Writing_review/writing_review_routes.py
+++ api/backend/Writing_review/writing_review_routes.py
@@ -10,13 +10,13 @@ write_review = Blueprint('write_review', __name__)
 @write_review.route('/write_review/<int:coopPosting_id>', methods=['POST'])
 def post_review():
     cursor = db.get_db().cursor()
-    cursor.execute('''INSERT INTO feedback_posts (column1, column2, column3, ...)
-VALUES (value1, value2, value3, ...);
-;''')
+    cursor.execute('''INSERT INTO feedback_posts (coopPosting_id, student_Name, student_id, studentEmployee_id, returnOffer, 
+                   skillsLearned, challenges, writtenReview)
+                    VALUES (value1, value2, value3, ...);''')
     theData = cursor.fetchall()
     
     if not theData:
-        current_app.logger.warning("No data found in feedback_posts.")
+        current_app.logger.warning("Review not complete.")
     else:
         current_app.logger.info(f"Fetched {len(theData)} records.")
         
