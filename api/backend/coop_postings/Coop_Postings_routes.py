f99e147d5bbc449e47e441c49420a8dbc212e0e3
danielYZeng <166508723+danielYZeng@users.noreply.github.com>
2024-12-05 15:22:35 -0500

    Update Coop_Postings_routes.py
    added route to fetch co-op listings by which received the latest review/feedback.
    capitalized all the AT words in the createdAT and updatedAT attributes

--- api/backend/coop_postings/Coop_Postings_routes.py
+++ api/backend/coop_postings/Coop_Postings_routes.py
@@ -13,7 +13,7 @@ def get_coop_postings():
     cursor = db.get_db().cursor()
     cursor.execute('''SELECT coopPosting_id, company_id, jobTitle, jobDescription, location, jobType, pay, 
                            companyBenefits, startDate, endDate, linkToApply, requirements, hiringManagerEmail, 
-                           createdAt, updatedAt FROM coop_postings;''')
+                           createdAT, updatedAT FROM coop_postings;''')
     theData = cursor.fetchall()
     
     if not theData:
@@ -54,7 +54,7 @@ def get_coop_postings_by_date():
     cursor = db.get_db().cursor()
     cursor.execute('''SELECT coopPosting_id, company_id, jobTitle, jobDescription, location, jobType, pay, 
                            companyBenefits, startDate, endDate, linkToApply, requirements, hiringManagerEmail, 
-                           createdAt, updatedAt FROM coop_postings ORDER BY updatedAt;''')
+                           createdAT, updatedAT FROM coop_postings ORDER BY updatedAT;''')
     theData = cursor.fetchall()
     
     if not theData:
@@ -65,3 +65,22 @@ def get_coop_postings_by_date():
     the_response = make_response(jsonify(theData))
     the_response.status_code = 200
     return the_response
+
+# A ROUTE TO FETCH ALL CO-OP POSTINGS BY WHICH RECEIVED THE LATEST REVIEW. THIS IS FOR THE 4_Coops_By_Latest.Reviews.py PAGE
+@coop_postings.route('/coop_postings', methods=['GET'])
+def get_coop_postings_by_latest_review():
+    cursor = db.get_db().cursor()
+    cursor.execute('''SELECT coopPosting_id, company_id, jobTitle, jobDescription, location, jobType, pay, 
+                           companyBenefits, startDate, endDate, linkToApply, requirements, hiringManagerEmail, 
+                           createdAT, updatedAT FROM coop_postings cp JOIN feedback_posts fp 
+                           ON cp.coopPosting_id = fp.coopPosting.id ORDER BY fp.updatedAT;''')
+    theData = cursor.fetchall()
+    
+    if not theData:
+        current_app.logger.warning("No data found in coop_postings.")
+    else:
+        current_app.logger.info(f"Fetched {len(theData)} records.")
+        
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response    
