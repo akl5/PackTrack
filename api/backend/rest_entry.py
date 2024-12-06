2cf44d43a1d3de28c93a0732c521d71cabf4fcf4
Alice Lee <86637906+akl5@users.noreply.github.com>
2024-12-06 12:12:04 -0500

    route issue fixes

--- api/backend/rest_entry.py
+++ api/backend/rest_entry.py
@@ -4,7 +4,7 @@ from backend.db_connection import db
 from backend.coop_postings.Coop_Postings_routes import coop_postings
 from backend.feedback_posts.feedback_posts_routes import feedback_posts
 from backend.system_diagnostics.system_diagnostics import system_diagnostics
-from backend.writing_review.writing_review_routes import write_review
+from backend.writing_review_routes.writing_review_routes import write_review
 
 import os
 from dotenv import load_dotenv
