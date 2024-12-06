33417892834a473cd421722812e77b030e7a317f
Alice Lee <86637906+akl5@users.noreply.github.com>
2024-12-06 02:01:20 -0500

    created system diagnostics page for system admin

---
+++ api/backend/system_diagnostics/system_diagnostics.py
@@ -0,0 +1,23 @@
+from flask import Blueprint
+from flask import request
+from flask import jsonify
+from flask import make_response
+from flask import current_app
+from backend.db_connection import db
+
+system_diagnostics = Blueprint('system_diagnostics', __name__)
+
+@system_diagnostics.route('/system_diagnostics', methods=['GET'])
+def get_system_diagnostics():
+    cursor = db.get_db().cursor()
+    cursor.execute('''SELECT * FROM system_diagnostics;''')
+    theData = cursor.fetchall()
+    
+    if not theData:
+        current_app.logger.warning("No data found in feedback_posts.")
+    else:
+        current_app.logger.info(f"Fetched {len(theData)} records.")
+        
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+    return the_response
